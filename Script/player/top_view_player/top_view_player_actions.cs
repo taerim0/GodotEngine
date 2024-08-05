c31b3d0dfff1939b0a5ffb0a3702e637223ca47d
taerim0 <taerim@ajou.ac.kr>
2024-08-05 17:45:50 +0900

    Add : player interaction area logic

--- Script/player/top_view_player/top_view_player_actions.cs
+++ Script/player/top_view_player/top_view_player_actions.cs
@@ -3,27 +3,70 @@ using System;
 
 public partial class top_view_player_actions : CharacterBody2D
 {
-	private int speed = 400;
+	private int speed;
+
+	// PlayerData
+	private int NormalSpeed;
+	private int RunSpeed;
+
+	// Manager
+	private game_manager gameManager;
+
+	// ChildNode
+	private Area2D interactionArea;
+	private CollisionShape2D interactionAreaCollision;
+
+	// InputMap
+	private string moveLeftKey = "TopViewPlayerMovement_Left";
+	private string moveRightKey = "TopViewPlayerMovement_Right";
+	private string moveUpKey = "TopViewPlayerMovement_Up";
+	private string moveDownKey = "TopViewPlayerMovement_Down";
+
+	private string interactionKey = "TopViewPlayerInteraction";
 
 	public override void _Ready()
 	{
+		gameManager = GetNode<game_manager>("../game_manager");
+
+		interactionArea = GetNode<Area2D>("interaction_area");
 	}
 
 	public override void _Process(double delta)
 	{
+
+		UpdatePlayerData();
 		PlayerMovements(delta);
+	}
+
+	private void UpdatePlayerData()
+	{
+		NormalSpeed = (int)gameManager.tmpPlayerDataResource.TopViewNormalSpeed;
+		RunSpeed = (int)gameManager.tmpPlayerDataResource.TopViewRunSpeed;
 	}
 
 	private void PlayerMovements(double delta)
 	{
 		// Run
 		if (Input.IsKeyPressed(Key.Shift))
-			speed = 600; else speed = 400;
+			speed = RunSpeed; else speed = NormalSpeed;
 
 		// 8-Way Movements
-		Vector2 velocity = Input.GetVector("TopViewPlayerMovement_Left", "TopViewPlayerMovement_Right", "TopViewPlayerMovement_Up", "TopViewPlayerMovement_Down");
+		float velocityX = Input.GetAxis(moveLeftKey, moveRightKey);
+		float velocityY = Input.GetAxis(moveUpKey, moveDownKey);
+
+		if (Input.IsActionPressed(interactionKey))
+		{
+			utils.UpdateInterationAreaDir(interactionArea, new Vector2(velocityX, velocityY));
+			objectInteraction();
+		}
 
 		// Apply Movements to Character
-		var collision = MoveAndCollide(velocity * speed * (float)delta);
+		MoveAndCollide(new Vector2(velocityX, 0) * speed * (float)delta);
+		MoveAndCollide(new Vector2(0, velocityY) * speed * (float)delta);
+	}
+
+	private void objectInteraction()
+	{
+
 	}
 }
