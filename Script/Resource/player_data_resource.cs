b6a2ec3b824e4b9554021752454d57abfc229190
taerim0 <taerim@ajou.ac.kr>
2024-08-05 17:46:19 +0900

    Add : playerData itemInventory

--- Script/Resource/player_data_resource.cs
+++ Script/Resource/player_data_resource.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Godot.Collections;
 using System;
 
 public partial class player_data_resource : Resource
@@ -6,15 +7,32 @@ public partial class player_data_resource : Resource
 	// base
 	[Export]
 	public string playerName { get; set; }
+    [Export]
+    public int playerLevel { get; set; }
 
-	// TopViewData
+    [Export]
+    public int saveFileID { get; set; }
+
+    // TopViewData
+    [Export]
+    public float TopViewNormalSpeed { get; set; }
 	[Export]
-	public float TopViewSpeed { get; set; }
-
+    public float TopViewRunSpeed { get; set; }
 
 	// SideViewData
 	[Export]
 	public float SideViewSpeed { get; set; }
 	[Export]
 	public float SideViewJumpSpeed { get; set; }
+
+	// Equipments
+
+
+	// Inventory
+	[Export]
+	public Array<item_resource> itemInventory { get; set; }
+    [Export]
+    public Array<Dictionary<int, int>> itemAmount { get; set; }
+    [Export]
+    public Array<Dictionary<int, int>> itemPosition { get; set; }
 }
